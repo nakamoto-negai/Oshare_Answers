379db9b16aa98bd5c23166333b6e1414850d1880
nakamoto_negai <md25009@cc.it-hiroshima.ac.jp>
2025-06-02 17:14:34 +0900

    サインインログイン機能を実装しました

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,37 @@ from purchases.models import Purchase  # または orders.models.Order など、
 
 User = get_user_model()
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import SignUpForm, SignInForm
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # ホーム画面などにリダイレクト
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def signin_view(request):
+    if request.method == 'POST':
+        form = SignInForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignInForm()
+    return render(request, 'accounts/signin.html', {'form': form})
+
+def signout_view(request):
+    logout(request)
+    return redirect('signin')
+
+
 @login_required
 def profile(request, user_id):
     user = get_object_or_404(User, pk=user_id)
